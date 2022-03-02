1a7f26383865e7322014b7566975a58c26ca9705
Raulea <robert.raulea@bearingpoint.com>
2022-03-02 17:30:23 +0200

    Implementation of search with PrimeNg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {AccordionModule} from 'primeng/accordion';     //accordion and accordion tab
-import {ButtonModule} from 'primeng/button';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AccordionModule } from 'primeng/accordion';     //accordion and accordion tab
+import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
+
+import { InputTextModule } from 'primeng/inputtext';
 
 
-
-import { AppRoutingModule } from './app-routing.module';
+// Components imports
+import { AppRoutingModule } from './app-routing.module';  // --> Router Module
 import { AppComponent } from './app.component';
 import { DogsComponent } from './dogs/dogs.component';
 import { BreedComponent } from './breed/breed.component';
 import { SubBreedComponent } from './sub-breed/sub-breed.component';
+import { SearchFilterPipe } from './search-filter.pipe'; // --> Search Pipe
+import { HighlightDirective } from './highlight.directive'; 
+
 
 @NgModule({
   declarations: [
@@ -19,7 +25,8 @@ import { SubBreedComponent } from './sub-breed/sub-breed.component';
     DogsComponent,
     BreedComponent,
     SubBreedComponent,
-    
+    SearchFilterPipe,  // -> added filter pipe to use it inside the component
+    HighlightDirective,
   ],
   imports: [
     BrowserModule,
@@ -27,7 +34,9 @@ import { SubBreedComponent } from './sub-breed/sub-breed.component';
     HttpClientModule,
     NgbModule,
     AccordionModule,
-    ButtonModule
+    ButtonModule,
+    FormsModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/dogs/dogs.component.ts
+++ src/app/dogs/dogs.component.ts
@@ -9,6 +9,7 @@ import { ApiService } from '../api.service';
 export class DogsComponent implements OnInit {
 
   breeds: string[] = [];
+  searchText: string = '';
 
   constructor(private readonly apiService: ApiService) { }
 
