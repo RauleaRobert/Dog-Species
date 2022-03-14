70ee8e040ce913c7fcb56d3c565b4de306893102
Raulea <robert.raulea@bearingpoint.com>
2022-03-14 17:56:47 +0200

    Search highlight + subBreeds list

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -6,12 +6,11 @@ import { Directive , Input, ElementRef, Renderer2, OnChanges, SimpleChanges} fro
 export class HighlightDirective implements OnChanges {
 
   @Input('appHighlight') searchedWord: string =''; // searchedWord
-  
-  name: string = ''
 
   constructor(private el: ElementRef, private renderer: Renderer2) { }
   
   ngOnChanges(changes: SimpleChanges): void {
+    // console.log(this.el.nativeElement.innerText);
     if(!changes['searchedWord'].firstChange){
       this.replaceWithHighlight(this.searchedWord.toLocaleLowerCase(), this.el.nativeElement.innerText.toLocaleLowerCase())
       
@@ -19,12 +18,20 @@ export class HighlightDirective implements OnChanges {
   }
 
   replaceWithHighlight(searchWord: string, element: string){
+    
     if(searchWord){
       if(element !== ""){
-        // this.divideSubBreeds(element); -> Please ignore this changes in commit
-        // console.log(this.divideSubBreeds(element));
-        element = element.replace(searchWord , '<span class="myClass" >' + searchWord + '</span>' );
-        this.changeText(this.capitalizeFirstLetter(element))
+        let subBreedItems = this.divideSubBreeds(this.el.nativeElement.innerText); 
+          element = element.replace(searchWord , '<span class="myClass" >' + searchWord + '</span>' );
+          element = element.split('\n')[0].toString();
+            if(subBreedItems.length > 0){
+              element = element + '<ul>';
+                for(let i = 0; i < subBreedItems.length; i++){
+                  element = element + '<li>'  + subBreedItems[i] + '</li>';
+                };
+              element = element + '</ul>';
+           };
+        this.changeText(this.capitalizeFirstLetter(element));
       }
     }else {
       this.deleteSpanClass(this.el.nativeElement.children);
@@ -34,32 +41,30 @@ export class HighlightDirective implements OnChanges {
   deleteSpanClass(htmlCollection: any){
     for(let i =0; i < htmlCollection.length; i++){
       if(htmlCollection[i].tagName === 'SPAN'){
-        this.removeCls(htmlCollection[i]) 
+        this.removeCls(htmlCollection[i]);
       }
     }
   }
 
-  // -> Please ignore following changes in commit
+  divideSubBreeds(elements: string){ // first element is the brees and the rest are the SubBreeds 
+    let subBreedsList = elements.split("\n");
+    return this.removeFirstElement(subBreedsList);
+  }
 
-  // divideSubBreeds(elements: string){ // first element is the brees and the rest are the SubBreeds 
-  //   let subBreedsList = elements.split(" ");
-  //   return this.removeFirstElement(subBreedsList);
-  // }
-
-  // removeFirstElement(arr : any){
-  //   arr.shift();
-  //   return arr;
-  // }
+  removeFirstElement(arr : any){
+    arr.shift();
+    return arr;
+  }
 
   capitalizeFirstLetter(string : string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
   changeText( text: string) {
-      this.renderer.setProperty(this.el.nativeElement, 'innerHTML', text)
+      this.renderer.setProperty(this.el.nativeElement, 'innerHTML', text);
   }
 
   removeCls(element: string){
-    this.renderer.removeClass( element , 'myClass')
+    this.renderer.removeClass( element , 'myClass');
   }
 }
